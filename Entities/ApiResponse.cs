8f1d41dbc91128e19d3ccbf8aed304aee280ecb2
Sneha S <Sneha.S@lntinfotech.com>
2022-11-14 23:28:25 +0530

    del

--- Entities/ApiResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Entities
-{
-    public class ApiResponse<T>
-    {
-        public T Result { get; set; }
-        public bool Success { get; set; }
-        public string Message { get; set; }
-    }
-}
